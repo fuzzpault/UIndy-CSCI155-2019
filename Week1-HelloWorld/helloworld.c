bcb4817860b59f4da9184f6363708244b215cc57
EC2 Default User <ec2-user@ip-172-31-20-157.us-east-2.compute.internal>
2019-08-29 14:33:31 +0000

    hello world

---
+++ Week1-HelloWorld/helloworld.c
@@ -0,0 +1,13 @@
+/*
+    Name: Paul Talaga
+    Date: Aug 29, 2019
+    Desc: My first program which outputs hello world.
+*/
+
+#include "stdio.h"
+
+int main(){
+    printf("Hello world!");
+    
+    return 0;
+}
