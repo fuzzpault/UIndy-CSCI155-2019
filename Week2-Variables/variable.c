17bdba70a699074bd23901daa51fa7fa1204821d
Paul Talaga <talagap@uindy.edu>
2019-09-04 13:55:37 +0000

    variable update

---
+++ Week2-Variables/variable.c
@@ -0,0 +1,18 @@
+/*
+    Name: Paul Talaga
+    Date: Sept 4, 2019
+    Desc: A demonstration of changing a variable.
+*/
+
+#include "stdio.h"
+
+int main(){
+    
+    int a = 10;
+    printf("a: %d \n", a);
+    a = 5;
+    printf("a: %d \n", a);
+        
+    
+    return 0;
+}
