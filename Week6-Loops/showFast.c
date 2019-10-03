d1d141c19f6ff9e48c58e4ea6f4ba40502c87ded
Steve Spicklemire <spicklemire@uindy.edu>
2019-10-03 10:12:39 +0000

    add showFast.c illustrating what happens when you re-seed too quickly.

---
+++ Week6-Loops/showFast.c
@@ -0,0 +1,20 @@
+/*
+    Name: Steve Spicklemire
+    Date: Oct 3, 2019
+    Desc: What happens when you run again quickly?
+*/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+int main(){
+    
+    // This makes sure rand() gives different numbers every time the 
+    // program is run.  It starts in the 'file' based on the current time.
+    srand(time(NULL)); // seed random number generator
+    
+    printf("I have a random number for you! %u\n", rand());
+
+    return 0;
+}
