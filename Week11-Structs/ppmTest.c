aa50eb7ce7ace9e3cbe4e85995b686df0e057a40
Steve Spicklemire <steve@spvi.com>
2019-11-06 09:51:20 -0500

    struct examples

---
+++ Week11-Structs/ppmTest.c
@@ -0,0 +1,63 @@
+/*
+Pointers to Structures.
+Steve Spicklemire
+Nov 4, 2019
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXWIDTH  255
+#define MAXHEIGHT 255
+
+typedef struct _pixel {
+    int red;
+    int green;
+    int blue;
+} Pixel;
+
+typedef struct _imagePPM {
+    char magic[3];  // magic identifier, "P3" for PPM
+    int width;      // number of columns
+    int height;     // number of rows
+    int max_value;  // maximum intensity of RGB components
+    Pixel pixels[MAXWIDTH][MAXHEIGHT]; // the actual color pixel data
+} ImagePPM;
+
+int main() {
+    
+    ImagePPM myImage;
+    int row = 0;
+    int col = 0;
+    int pixVal = 0;
+    
+    scanf("%s", myImage.magic);
+    scanf("%d", &(myImage.width));
+    scanf("%d", &(myImage.height));
+    scanf("%d", &(myImage.max_value));
+    
+    printf("%s\n", myImage.magic);
+    printf("%d %d %d\n", 
+                    myImage.width, 
+                    myImage.height, 
+                    myImage.max_value);
+                    
+    for (row = 0; row < myImage.width; row++) {
+        for (col = 0; col < myImage.height; col++) {
+            scanf("%d", &pixVal); // scan in red
+            myImage.pixels[col][row].red = pixVal;
+            scanf("%d", &pixVal); // scan in green
+            myImage.pixels[col][row].green = pixVal;
+            scanf("%d", &pixVal); // scan in blue
+            myImage.pixels[col][row].blue = pixVal;
+        }
+    }
+    
+    for (row = 0; row < myImage.width; row++) {
+        for (col = 0; col < myImage.height; col++) {
+            printf("%d\n",myImage.pixels[col][row].red);
+            printf("0\n0\n");
+        }
+    }
+        return 0;
+}
